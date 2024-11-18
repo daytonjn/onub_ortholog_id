a0eb663d5f963de4de6b74d6c34c54b146c284d5
daytonjn <dayton.jacob.n@gmail.com>
2024-11-18 13:20:04 -0500

    updated Onub2Dmel.sh and ran

--- scripts/onub2dmel.sh
+++ scripts/onub2dmel.sh
@@ -21,6 +21,4 @@ module load blast-plus/2.11.0
 #BLAST+ from Onub query to Dmel reference database
 
 blastp -query ${supporting_file_dir}Onub_peptides.fasta -db ${supporting_file_dir}Dmel-db \
--out ${output_dir}Onub2Dmel_blastp.txt -evalue 0.0001 -outfmt 6
-
-#-max_target_seqs 1 -max_hsps 1
+-max_target_seqs 1 -max_hsps 1 -out ${output_dir}Onub2Dmel_blastp.txt -evalue 0.0001 -outfmt 6
