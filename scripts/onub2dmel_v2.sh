6b67c60cd3d5c9369d87816275d4df9f6751f6c0
daytonjn <dayton.jacob.n@gmail.com>
2024-11-19 10:04:23 -0500

    relaxed blastp parameters to return more values, _v1 blastp and RBH python returned only ~50% orthologs

---
+++ scripts/onub2dmel_v2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH -p batch
+#SBATCH -o out_Onub2Dmel_v2.txt
+#SBATCH -e err_Onub2Dmel_v2.txt
+#SBATCH --mem=24gb
+#SBATCH --time=0-16:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jacob.dayton@tufts.edu
+
+#path to RefSeq peptide fasta files from NCBI and their BLAST+ db location
+supporting_file_dir="/cluster/tufts/dopmanlab/Jacob/onub_ortholog_id/supporting_files/"
+output_dir="/cluster/tufts/dopmanlab/Jacob/onub_ortholog_id/output/"
+
+mkdir ${output_dir}
+
+#load BLAST+ module
+module load blast-plus/2.11.0
+
+#BLAST+ from Onub query to Dmel reference database
+
+blastp -query ${supporting_file_dir}Onub_peptides.fasta -db ${supporting_file_dir}Dmel-db \
+-max_target_seqs 5 -max_hsps 1 -out ${output_dir}Onub2Dmel_blastp_v2.txt -evalue 0.001 -outfmt 6
